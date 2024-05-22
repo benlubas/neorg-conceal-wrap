129a98f083faa16cec934c308497adef4138ac19
Ben Lubas <56943754+benlubas@users.noreply.github.com>
2024-05-21 22:24:05 -0400

    fix: off by ones and performance bump (#2)
    * fix: various off by one issues
    
    * fix: remaining off by ones + slight perf bump
    
    * chore: remove dead code

--- lua/neorg/modules/external/conceal-wrap/module.lua
+++ lua/neorg/modules/external/conceal-wrap/module.lua
@@ -18,11 +18,19 @@ end
 
 module.load = function()
     local ns = vim.api.nvim_create_augroup("neorg-conceal-wrap", { clear = true })
+
+    module.private.break_at = vim.iter(vim.split(vim.o.breakat, ""))
+        :filter(function(x)
+            return not vim.list_contains(module.config.private.no_break_at, x)
+        end)
+        :totable()
+
     vim.api.nvim_create_autocmd("BufEnter", {
         desc = "Set the format expression on norg buffers",
         pattern = "*.norg",
         group = ns,
         callback = function(ev)
+            -- set the format expression for the buffer.
             vim.api.nvim_set_option_value(
                 "formatexpr",
                 "v:lua.require'neorg.modules.external.conceal-wrap.module'.public.format()",
@@ -34,39 +42,31 @@ end
 
 module.config.public = {}
 
----take an index, and a list that represents lines, and return the real row and column of that
----index. Returns nil when i is out of range
----@param lines string[] the lines with their leading white space removed
----@param i number 0 based index
----@return {[1]: number, [2]: number}
-module.private.index_to_rc = function(lines, i)
-    local remaining_index = i
-    for current_row, line in ipairs(lines) do
-        if line:len() >= remaining_index then
-            return { current_row, remaining_index }
-        end
-        remaining_index = remaining_index - line:len() - 1
-    end
-    log.error(
-        "[neorg-conceal-wrap] index out of range in `index_to_rc` (this shouldn't ever happen, please open a bug)."
-    )
-    return { -1, -1 }
-end
+module.config.private = {}
 
----take a row, col tuple and return the index into a string of lines
----@param lines string[]
----@param pos {[1]: number, [2]: number}
----@return number 0 based index into the string
-module.private.rc_to_index = function(lines, pos)
-    local idx = 0
-    for i = 1, pos[1] do
-        if i < pos[1] then
-            idx = idx + lines[i]:len() + 1
-        else
-            idx = idx + pos[2] -- not this + 1
-        end
-    end
-    return idx
+---Chars that we remove from break-at when wrapping lines. Break-at is global, and we don't want to
+---mess with it. We will respect it until it starts to break syntax... Hmm. these are all valid in
+---between words though, so maybe we could check that? Like this/that is fine, and doesn't start an
+---italic section. so it would be okay to break there. Then we also have to consider when they touch
+---against new lines though, that's annoying too. I think I will just remove them from breakat for
+---now then.
+module.config.private.no_break_at = { "/", ",", "!", "-", "*" }
+
+---join lines defined by the 0 index start and end into a single line. Lines are separated by single
+---spaces.
+---@param buf number
+---@param start number 0 based start
+---@param _end number 0 based exclusive end
+module.private.join_lines = function(buf, start, _end)
+    local og_lines = vim.api.nvim_buf_get_lines(buf, start, _end, false)
+    local joined = vim.iter(og_lines)
+        :map(function(x)
+            x = x:gsub("^%s+", "")
+            x = x:gsub("%s+$", "")
+            return x
+        end)
+        :join(" ")
+    vim.api.nvim_buf_set_lines(buf, start, _end, false, { joined })
 end
 
 ---Function to be used as `:h 'formatexpr'` which will hard wrap text in such a way that lines will
@@ -84,55 +84,41 @@ module.public.format = function()
         local buf = vim.api.nvim_get_current_buf()
         local current_row = vim.v.lnum - 1
         ---kinda like a byte index, It's just how far we are in the string of text.
-        local col_index = 1
-        local og_lines = vim.api.nvim_buf_get_lines(buf, current_row, current_row + vim.v.count, false)
+        local col_index = 0
+        module.private.join_lines(buf, current_row, current_row + vim.v.count)
+        local line = vim.api.nvim_buf_get_lines(buf, current_row, current_row + 1, false)[1]
         local new_lines = {}
 
         local width = vim.bo.textwidth
 
-        local left_offset = 0
         -- account for breakindent
-        local leading_white_space = og_lines[1]:match("^%s*")
-        if leading_white_space then
-            left_offset = #leading_white_space
-        end
-        og_lines = vim.iter(og_lines)
-            :map(function(l)
-                local ret = string.gsub(l, "^%s+", "")
-                ret = string.gsub(ret, "%s+$", "")
-                return ret
-            end)
-            :totable()
+        local indent = vim.fn.eval(vim.bo.indentexpr)
 
-        local line = table.concat(og_lines, " ")
+        local left_offset = indent
 
         width = math.max(width - left_offset, 5) -- arbitrary 5 char limit
         while #line > 0 do
-            local s = module.private.index_to_rc(og_lines, col_index)
-            local e = module.private.index_to_rc(og_lines, col_index + #line - 1)
-            s = { s[1] + current_row - 1, s[2] + #leading_white_space }
-            e = { e[1] + current_row - 1, e[2] + #leading_white_space }
-            local visible_width, next_cutoff_rc = module.private.visible_text_width(buf, s, e, width)
-            local next_index =
-                module.private.rc_to_index(og_lines, { next_cutoff_rc[1] - current_row + 1, next_cutoff_rc[2] })
+            local visible_width, next_cutoff_index =
+                module.private.visible_text_width(buf, current_row, col_index, col_index + #line, width)
+
             if visible_width <= width then
                 table.insert(new_lines, line)
                 break
             end
             -- definitely need this + 1 right now
-            local chunk = line:sub(0, next_index - col_index + 1)
-            --NOTE: The error is probably happening before this, right?
+            -- what is this + 1 for though?
+            local chunk = line:sub(0, next_cutoff_index - col_index + 1)
 
             local i = #chunk
             while i > 0 do
-                if vim.tbl_contains(vim.split(vim.o.breakat, ""), chunk:sub(i, i)) then
+                if vim.list_contains(module.private.break_at, chunk:sub(i, i)) then
                     break
                 end
                 i = i - 1
             end
             if i == 0 then
                 -- we didn't find a space, so we just break the line at the width
-                i = next_index
+                i = next_cutoff_index
             end
             table.insert(new_lines, line:sub(0, i))
             col_index = col_index + i
@@ -141,13 +127,13 @@ module.public.format = function()
 
         new_lines = vim.iter(new_lines)
             :map(function(l)
-                l = l:gsub("^%s*", leading_white_space)
+                l = l:gsub("^%s*", (" "):rep(indent))
                 l = l:gsub("%s+$", "")
                 return l
             end)
             :totable()
         -- Now we have new lines, have to write them to the buffer
-        vim.api.nvim_buf_set_lines(buf, vim.v.lnum - 1, vim.v.lnum + vim.v.count - 1, false, new_lines)
+        vim.api.nvim_buf_set_lines(buf, current_row, current_row + 1, false, new_lines)
         return 0
     end)
     if not ok then
@@ -159,16 +145,23 @@ end
 ---Compute the "visible" width of the given range, that is the width of the line when conceal is active.
 ---If the visible width is larger than target, return the position of the last visible character before target.
 ---@param buf number
----@param start {[1]: number, [2]: number}
----@param _end {[1]: number, [2]: number}
+---@param line_idx number 0 based line number
+---@param start_col number 0 based
+---@param end_col number 0 based, exclusive
 ---@param target number
----@return number, {[1]: number, [2]: number}
-module.private.visible_text_width = function(buf, start, _end, target)
+---@return number width, number next_index
+module.private.visible_text_width = function(buf, line_idx, start_col, end_col, target)
     -- offset x by inline virtual text
     local width = 0
     -- track positions that are concealed by extmarks
     local extmark_concealed = {}
-    local same_line_extmarks = vim.api.nvim_buf_get_extmarks(buf, -1, start, _end, { details = true })
+    local same_line_extmarks = vim.api.nvim_buf_get_extmarks(
+        buf,
+        -1,
+        { line_idx, start_col },
+        { line_idx, end_col },
+        { details = true }
+    )
     for _, extmark in ipairs(same_line_extmarks) do
         local details = extmark[4]
         -- we don't care if conceal is on or off. Always wrap for when it's on
@@ -181,40 +174,23 @@ module.private.visible_text_width = function(buf, start, _end, target)
         end
     end
 
-    local best_target = start
-    for r = start[1], _end[1] do
-        local line = vim.api.nvim_buf_get_lines(buf, r, r + 1, false)[1]
-        local leading_white_space = line:match("^%s*")
-        local start_c = #leading_white_space + 1
-        local end_c = line:len()
-        if r == start[1] then
-            start_c = start[2]
-        end
-        if r == _end[1] then
-            end_c = _end[2]
-        end
-        for c = start_c, end_c do
-            width = width + 1
-            local res = vim.inspect_pos(
-                buf,
-                r,
-                c,
-                { semantic_tokens = false, syntax = false, extmarks = false, treesitter = true }
-            )
-            for _, hl in ipairs(res.treesitter) do
-                if hl.capture == "conceal" and not extmark_concealed[c + 1] then
-                    width = width - 1
-                    break
-                end
+    local best_target = start_col
+    -- col indexing is 0 based
+    for c = start_col, end_col - 1 do
+        width = width + 1
+        local res = vim.treesitter.get_captures_at_pos(buf, line_idx, c)
+        for _, hl in ipairs(res) do
+            if hl.capture == "conceal" and not extmark_concealed[c + 1] then
+                width = width - 1
+                break
             end
+        end
 
-            -- target + 1 b/c if the lines ends where "d" in "word " is at `target` then we should break at the " "
-            if width <= target + 1 then
-                best_target = { r, c }
-            end
+        -- target + 1 b/c if the lines ends where "d" in "word " is at `target` then we should break at the " "
+        if width <= target + 1 then
+            best_target = c
         end
     end
-    width = width - 1
     return width, best_target
 end
 
